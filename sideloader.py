6097f9e97c6fe4780001bdadc9dd29f7181e382c
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2022-02-18 17:59:37 +0000

    Version 1.1.0

--- sideloader.py
+++ sideloader.py
@@ -3,14 +3,14 @@ import os
 import PySimpleGUI as gui
 import platform
 
-version = "1.0.5"
+version = "1.1.0"
 
 def main():
     # Check if OS is Windows 11
     if int((platform.version().split('.')[2])) < 22000:
         layout = [[gui.Text('Sorry! WSA Sideloader will only run on Windows 11.')],
                 [gui.Exit()]]
-        window = gui.Window('Error', layout)
+        window = gui.Window('Unsupported OS', layout)
 
         event, values = window.Read()
         if event is None or "Exit":
@@ -35,13 +35,25 @@ def main():
             [gui.Input(),gui.FileBrowse(file_types=(("APK files","*.apk"),))],
             [gui.Text('ADB address:')],
             [gui.Input('127.0.0.1:58526')],
-            [gui.Button('Install')]]
+            [gui.Button('Install'),gui.Button('Installed apps')]]
 
     window = gui.Window('WSA Sideloader '+version, layout)
 
-    event, values = window.Read()
-    if event is None:
-        quit()
+    while True:
+        event, values = window.Read()
+        if event is None:
+            quit()
+        if event == "Installed apps":
+            address = values[1]
+            command = os.popen('cmd /c "cd platform-tools & adb connect '+address+' & adb shell am start -n "com.android.settings/.applications.ManageApplications""')
+            output = command.readlines()
+            check = str(output[len(output)-1])
+            if check.startswith("Starting: Intent { cmp=com.android.settings/.applications.ManageApplications }"):
+                pass
+            else:
+                gui.SystemTray.notify('Unable to perform this operation', 'Please check that WSA is running and the correct ADB address has been entered.',display_duration_in_ms=5000,icon=None)
+        if event == "Install":
+            break
 
     source_filename = values[0]
     address = values[1]
@@ -57,15 +69,23 @@ def main():
     window.Close()
     if check.startswith("Success"):
         layout = [[gui.Text('The application has been successfully installed.')],
-                [gui.Exit()]]
+                [gui.Exit(),gui.Button('Install another APK')]]
         window = gui.Window('Information', layout)
 
         event, values = window.Read()
-        window.Close()
+        if event == "Install another APK":
+            window.Close()
+            main()
+        else:
+            quit()
     else:
         layout = [[gui.Text('WSA Sideloader could not install the application. Please check that:\nThe APK file is valid\nWSA is running\nDev mode is enabled and the correct address has been entered')],
-                [gui.Exit()]]
-        window = gui.Window('Information', layout)
+                [gui.Button('OK')]]
+        window = gui.Window('Error', layout)
 
         event, values = window.Read()
-        window.Close()
+        if event == "OK":
+            window.Close()
+            main()
+        else:
+            quit()
