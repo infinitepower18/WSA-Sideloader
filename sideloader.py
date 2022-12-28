95c96ebda6dcdd53b5da44e9b2716f9e70c265ee
Ahnaf Mahmud <44692189+infinitepower18@users.noreply.github.com>
2022-12-28 00:15:34 +0000

    Update sideloader.py

--- sideloader.py
+++ sideloader.py
@@ -82,8 +82,7 @@ def start(filearg = ""): # For GitHub installs
     configpath = os.getenv('LOCALAPPDATA') + "\\WSA Sideloader\\config.ini"
     try:
         response = requests.get("https://api.github.com/repos/infinitepower18/WSA-Sideloader/releases/latest")
-        latestver = response.json()["name"]
-        latestver = latestver[16:]
+        latestver = response.json()["tag_name"]
         if parse_version(latestver) > parse_version(version):
             layout = [[gui.Text('A newer version of WSA Sideloader is available.\nWould you like to update now?',font=("Calibri",11))],
                 [RoundedButton("Yes",0.3,font="Calibri 11"),RoundedButton("No",0.3,font="Calibri 11")]]
