948255601310aa8ea2b682546aa09a4974229912
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2023-06-24 17:29:01 +0100

    rewrite installed apps code

--- sideloader.py
+++ sideloader.py
@@ -355,9 +355,14 @@ def main():
                         address = adbAddress
                         address = address.replace(" ", "")
                         adbRunning = True
-                        subprocess.Popen(adbApp + " connect "+address)   
-                        command = subprocess.Popen(adbApp + ' -s '+address+' shell am start -n "com.android.settings/.applications.ManageApplications"',stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding='utf-8',creationflags=0x08000000)
-                        output = command.stdout.readlines()
+                        connCommand = subprocess.Popen(adbApp + " connect "+address,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding='utf-8',creationflags=0x08000000)
+                        output, stderr = connCommand.communicate()
+                        output = output.splitlines()
+                        stderr = stderr.splitlines()
+                        if output[-1].startswith("connected") or output[-1].startswith("already connected"): 
+                            command = subprocess.Popen(adbApp + ' -s '+address+' shell am start -n "com.android.settings/.applications.ManageApplications"',stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding='utf-8',creationflags=0x08000000)
+                            output, stderr = command.communicate()
+                            output = output.splitlines()
                         check = str(output[len(output)-1])
                         if check.startswith("Starting: Intent { cmp=com.android.settings/.applications.ManageApplications }"):
                             window["_ERROR2_"].Update(visible=False)
