3bff89853c08f9eca899ec4dfaf6eb0cbc2cdb31
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2024-08-24 13:21:16 +0100

    Refine theme

--- sideloader.py
+++ sideloader.py
@@ -58,12 +58,14 @@ if darkdetect.isDark():
     gui.theme_background_color("#232020")
     gui.theme_text_element_background_color("#232020")
     gui.theme_text_color("White")
-    gui.theme_button_color(('#232020', winaccent.accent_dark))
-    gui.theme_input_background_color('#ADD8E6')
-    gui.theme_input_text_color('#000000')
+    gui.theme_button_color(('Black', winaccent.accent_dark))
+    gui.theme_input_background_color("#5d5656")
+    gui.theme_input_text_color('White')
 else:
     gui.theme("LightGrey")
     gui.theme_button_color(("White", winaccent.accent_light))
+    gui.theme_input_background_color("#5d5656")
+    gui.theme_input_text_color('White')
 
 def startgit(filearg = ""):
     global installsource
